512158505a5648fb22774e45c882d135080d81fd
heinhtet405 <heinhtet.hh405@gmail.com>
2017-09-14 19:17:51 +0630

    Complete Project

--- DiceRoller/ViewController.swift
+++ DiceRoller/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
 
+    @IBOutlet weak var dicImageOne: UIImageView!
+    @IBOutlet weak var dicImageTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generateRandomData()
+    }
+    
+    // Hide Status Bar
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pressedRollBtn(_ sender: Any) {
+        generateRandomData()
     }
-
-
+    
+    func generateRandomData() {
+        // Generate Random Number
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        // Add Image
+        dicImageOne.image = UIImage(named: diceArray[randomDiceIndex1])
+        dicImageTwo.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
 }
 
